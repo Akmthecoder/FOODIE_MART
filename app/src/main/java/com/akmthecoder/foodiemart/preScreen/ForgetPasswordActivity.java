fc6a84f63bb26341168e893cf8c29406d38d268f
Akmthecoder <1999anilkumarmaurya@gmail.com>
2021-10-25 14:02:33 +0530

    sign in & forget password screen!!

---
+++ app/src/main/java/com/akmthecoder/foodiemart/preScreen/ForgetPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.akmthecoder.foodiemart.preScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.akmthecoder.foodiemart.R;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+}
